395c9b716c00e6e077056ad166116b2da944beb1
Steven De Kock <sdekock87+github@gmail.com>
2012-05-28 17:08:37 +0200

    Adding a test for System.Windows.Controls assembly issues.

---
+++ src/DummyTests/VS2010/DummyTests.Silverlight5/ControlsTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DummyTests.Silverlight5
+{
+    [TestClass]
+    public class ControlsTest
+    {
+        [TestMethod]
+        public void TryInstantiateControl()
+        {
+            var button = new Button();
+            Assert.IsNotNull(button);
+        }
+    }
+}
