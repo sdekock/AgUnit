76fd425944edf56c44c28a796d186cf2640c9dad
Steven De Kock <sdekock87@gmail.com>
2013-11-03 23:36:45 +0100

    Bump version number.

--- src/AgUnit.Runner.Resharper80/Properties/ProjectInfo.cs
+++ src/AgUnit.Runner.Resharper80/Properties/ProjectInfo.cs
@@ -23,8 +23,8 @@ using JetBrains.Application.PluginSupport;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.7.0.0")]
-[assembly: AssemblyFileVersion("0.7.0.0")]
+[assembly: AssemblyVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("0.8.0.0")]
 
 // Plugin info for ReSharper
 
